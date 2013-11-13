e9d742e911133f73a02665652529700ec7acf12c
Nathan Yong <nathyong@gmail.com>
2013-11-13 20:01:12 +1100

    Process data for finesse and pieces per minute

--- analyse.r
+++ analyse.r
@@ -9,3 +9,11 @@ plot(x=linerace$achieved, y=linerace$time, type="p", ylab="Seconds", xlab="Time 
 lines(y=ma(linerace$time), x=linerace$achieved, type="l", col="blue")
 abline(lm(linerace$time ~ linerace$achieved), col="red")
 abline(h=min(linerace$time), lty=2)
+
+plot(x=linerace$achieved, y=linerace$ppm, type="p", ylab="Pieces", xlab="Time achieved", main="Pieces per minute")
+lines(y=ma(linerace$ppm), x=linerace$achieved, type="l", col="blue")
+abline(lm(linerace$ppm ~ linerace$achieved), col="red")
+
+plot(x=linerace$achieved, y=linerace$finesse, type="p", ylab="Excess keypresses", xlab="Time achieved", main="Finesse")
+lines(y=ma(linerace$finesse), x=linerace$achieved, type="l", col="blue")
+abline(lm(linerace$finesse ~ linerace$achieved), col="red")
